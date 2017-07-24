c38369f4982079def3ca13d90c65419221bd9599
Javier Perez <javierperezferrada@gmail.com>
2017-07-24 14:06:22 -0400

    advance json to products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from "./products.service";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  providers: [ProductsService]
 })
 export class ProductsComponent implements OnInit {
+  private sections: Array<any>;
 
-  constructor() { }
+  constructor(private _service: ProductsService) { }
 
   ngOnInit() {
+    this.sections = this._service.getSections();
   }
 
 }
