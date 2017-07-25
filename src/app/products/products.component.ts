01912d5478fe658e7e2ee734660a9f4b49328f57
Javier Perez <javierperezferrada@gmail.com>
2017-07-25 11:34:01 -0400

    added atention hours

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -18,28 +18,6 @@ export class ProductsComponent implements OnInit {
       element.principalProducts = element.products.slice(0,3);
       element.otherProducts = element.products.slice(3,);
     });
-
-
-    const PARTIES = [
-  { id: 1, title: "Main Event", description: "The biggest, most extravagant event in the last 10,000,000 years." },
-  { id: 2, title: "Secondary Event", description: "The not as biggest, less extravagant event in the last 10,000,000 years." },
-  { id: 3, title: "Another Event", description: "N/A" },
-  { id: 4, title: "Another Event", description: "N/A" },
-  { id: 5, title: "Another Event", description: "N/A" },
-  { id: 6, title: "Another Event", description: "N/A" },
-  { id: 7, title: "Another Event", description: "N/A" },
-  { id: 8, title: "Another Event", description: "N/A" },
-  { id: 9, title: "Another Event", description: "N/A" },
-  { id: 10, title: "Another Event", description: "N/A" }
-];
-
-var chunk_size = 3;
-const groups = PARTIES.map(function(e,i){
-    return i%chunk_size===0 ? PARTIES.slice(i,i+chunk_size) : null;
-})
-.filter(x=>!!x)
-
-console.log(groups);
   }
-
 }
+
