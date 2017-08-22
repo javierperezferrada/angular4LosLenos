ebbb4e8cd5aa49cada8b05224dab4bf80412c091
Javier Perez <javierperezferrada@gmail.com>
2017-08-22 11:45:59 -0300

    animation to ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component, ViewChild, HostListener, Inject } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/core';
 import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class AppComponent {
   title = 'app';
