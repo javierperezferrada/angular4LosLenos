1d7634239f2f5ebab123f6f764bf4f1b412c6f37
Javier Perez <javierperezferrada@gmail.com>
2017-08-24 13:38:08 -0300

    many changes

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-app-footer',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppFooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+    }, (reason) => {
+    });
+  }
+  
+  goGoogleMaps() {
+    window.open('https://www.google.com/maps/place/Restaurant+Los+Le%C3%B1os/@-39.81329,-73.243038,18z/data=!4m5!3m4!1s0x0:0xd4ef7deaa1acfa7e!8m2!3d-39.813307!4d-73.243056?hl=es-419');
+  }
+
+  goCall() {
+    window.open("tel:632231003");
   }
 
 }
